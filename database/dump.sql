3e3ab445e60d5f23456f5bfb255feee2ede71196
Jose421 <josevromero2000@gmail.com>
2026-04-18 15:20:22 -0400

    Update dump.sql

--- database/dump.sql
+++ database/dump.sql
@@ -181,12 +181,12 @@ ALTER TABLE ONLY public.bodega_encargado
     ADD CONSTRAINT fk_encargado FOREIGN KEY (encargado_id) REFERENCES public.encargado(id);
 
 
-INSERT INTO encargado (run, nombre, apellido1, apellido2, direccion, telefono)
+INSERT INTO public.encargado (run, nombre, apellido1, apellido2, direccion, telefono)
 VALUES
   ('28-9', 'Sebastian', 'Velazques', 'Contreras', 'Av. Siempre Viva 123','923456788'),
   ('98765432-1', 'Elisabeth', 'Beaufort', 'Swan', 'Calle Falsa 456', '987654321');
 
-INSERT INTO bodega (codigo, nombre, direccion, dotacion, estado)
+INSERT INTO public.bodega (codigo, nombre, direccion, dotacion, estado)
 VALUES
   ('B0012', 'Bodega Central', 'Santiago Centro', 10, 'Activada');
 
